845c1d506c80fba0a7d15328a95f55f88d4e7269
dKaustav43 <dkaustav43@gmail.com>
2026-02-07 22:52:09 +0000

    Wrapped the training and evaluation in a function.

--- scripts/training_and_eval.py
+++ scripts/training_and_eval.py
@@ -9,28 +9,35 @@ from sklearn.preprocessing import StandardScaler
 
 warnings.filterwarnings('ignore')
 
-df = load_data("data/data.csv")
+def training_and_eval(model_instance, path_to_data):
 
-df = df.drop(['id','Unnamed: 32'], axis=1)
+    df = load_data(path_to_data)
+    
+    df = df.drop(['id','Unnamed: 32'], axis=1)
+    
+    X = df.drop('diagnosis',axis=1)
+    y = df['diagnosis']
+    y = pd.get_dummies(y, drop_first=True, dtype=int)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+    
+    sc = StandardScaler()
+    X_train_scaled = sc.fit_transform(X_train)
+    X_test_scaled = sc.transform(X_test)
+    
+    Logistic_Regression = model_instance(C=1.5,l1_ratio=0.1,solver='saga')
+    Logistic_Regression.fit(X_train_scaled,y_train)
+    y_pred = Logistic_Regression.predict(X_test_scaled)
+    
+    eval_metrics, confusion_matrix = classification_metrics('Logistic Refression',y_test,y_pred)
+    
+    return eval_metrics, confusion_matrix
 
-X = df.drop('diagnosis',axis=1)
-y = df['diagnosis']
-y = pd.get_dummies(y, drop_first=True, dtype=int)
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+eval_metrics, confusion_matrix = training_and_eval(model_instance=LogisticRegression, path_to_data = "data/data.csv")
 
-sc = StandardScaler()
-X_train_scaled = sc.fit_transform(X_train)
-X_test_scaled = sc.transform(X_test)
-
-Logistic_Regression = LogisticRegression(C=1.5,l1_ratio=0.1,solver='saga')
-Logistic_Regression.fit(X_train_scaled,y_train)
-y_pred = Logistic_Regression.predict(X_test_scaled)
-
-final_eval_metrics, final_confusion_matrix = classification_metrics('Logistic Refression Final',y_test,y_pred)
-
-print(f"Eval metrics: \n {final_eval_metrics}\n")
-print(f"Confusion metrics: \n {final_confusion_matrix}")
+print(f"Eval metrics: \n {eval_metrics}\n")
+print(f"Confusion metrics: \n {confusion_matrix}")
 
 
 
