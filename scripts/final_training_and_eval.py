b0d642cc162efcb11142bd879962ea02e6be5fc0
dKaustav43 <dkaustav43@gmail.com>
2026-02-10 18:23:00 +0000

    Simplified the function.

--- scripts/final_training_and_eval.py
+++ scripts/final_training_and_eval.py
@@ -6,7 +6,7 @@ from modules.gridsearch_hyperparameter_tuning import gridsearch_bestestimator
 
 warnings.filterwarnings('ignore')
 
-def training_and_eval(final_model_name:str, model_instance_with_best_params, path_to_data:str):
+def training_and_eval(final_model_name:str, model_instance_with_best_params=gridsearch_bestestimator()):
 
     X_train_scaled, y_train, X_test_scaled, y_test = load_data_remove_columns_splitting_and_scaling_X_y_data()
     model_instance_with_best_params.fit(X_train_scaled,y_train)
@@ -19,10 +19,10 @@ def training_and_eval(final_model_name:str, model_instance_with_best_params, pat
 
 def main():
     
-    eval_metrics, confusion_matrix = training_and_eval(final_model_name = 'Logistic Regression', model_instance_with_best_params=gridsearch_bestestimator(), path_to_data = "data/data.csv")
+    eval_metrics, confusion_matrix = training_and_eval(final_model_name = 'Logistic Regression')
 
     print(f"Eval metrics: \n {eval_metrics}\n")
-    print(f"Confusion metrics: \n {confusion_matrix}")
+    print(f"Confusion matrix: \n {confusion_matrix}")
 
 if __name__ == "__main__":
     main()
