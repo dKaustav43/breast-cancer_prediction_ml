0e2a3e6ece1346418dd67faae873f3a75003bd5b
dKaustav43 <dkaustav43@gmail.com>
2026-08-20 10:16:34 +0100

    added doc string

--- src/modules/data_loading_preprocessing.py
+++ src/modules/data_loading_preprocessing.py
@@ -4,7 +4,20 @@ from sklearn.preprocessing import StandardScaler
 
 
 def load_data_remove_columns_splitting_and_scaling_X_y_data(path:str="data/data.csv"):
-    
+    """
+    Input: path to the data file as string.
+
+    Data transformation steps:
+
+    1. Loads the data in a dataframe.
+    2. Drops columns not needed for analysis.
+    3. Separates X and y data. 
+    4. Encodes y (diagnosis) data.
+    5. Splits the X data into training and testing. 
+    6. Scales the X data needed before training. 
+
+    Output: X_train_scaled, y_train, X_test_scaled, y_test
+    """
     df = pd.read_csv(path)
 
     df = df.drop(['id','Unnamed: 32'], axis=1)
